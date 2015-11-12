b3c2983cf1f0db44321db558933481680723631b
Robert Dockins <rdockins@galois.com>
2015-11-11 17:16:00 -0800

    Remove extracted files

--- src/extracted/ext/Bool.hs
+++
@@ -1,4 +0,0 @@
-module Bool where
-
-import qualified Prelude
-
--- src/extracted/ext/sf_spec.hs
+++
@@ -1,8 +0,0 @@
-module SF_spec where
-
-import qualified Prelude
-
-type Coq_rankSelection candidate = [] candidate
-
-type Coq_ballot candidate = [] (Coq_rankSelection candidate)
-
