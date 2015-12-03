217e578c79206c2dab5414648128b2a5aa64c240
Pat Hickey <pat@galois.com>
2015-12-02 17:15:01 -0800

    remove obsolete test case

--- src/extracted/rcvtest-shouldpass.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-cabal run rcv_testcase -- \
-	--rcv-path=../../../open-rcv-tests \
-	--case=6 \
-	--verbose \
-	echo '{"rounds":[{"elected":["Bob"],"totals":{"Bob":2,"Ann":0,"Carol":1}}]}'
